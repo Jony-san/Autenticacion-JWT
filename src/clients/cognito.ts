89b7b9777c8428547d39313b4c97e372fdab8355
Adryo-joni <jsantos@adryo.com.mx>
2026-02-12 21:33:36 -0600

    Add: acceso a variables de entorno

--- src/clients/cognito.ts
+++ src/clients/cognito.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const cognitoClient = new CognitoIdentityProviderClient({
   region: process.env.aws_region!,
 });
-
-
